c0ada3d523bc6f750c4d3404ad7be77983f008b1
Brett Peterson <bepeterson@petersonb.com>
2014-01-14 18:02:11 -0600

    api controller added

---
+++ application/controllers/api.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 */
+	public function index()
+	{
+		$this->load->view('api_message');
+	}
+
+
+	private function generate_key($size = null, $valid_chars = null)
+	{
+		$random_string = "";
+	}
+}
+
+/* End of file api.php */
+/* Location: ./application/controllers/api.php */
