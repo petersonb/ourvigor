0f0fa394df552651e2ea762084a3abcdffeb1d26
Brett Peterson <bepeterson@petersonb.com>
2014-01-23 15:40:28 -0600

    can now reference the "me" of the token with simple API

--- application/controllers/api/users.php
+++ application/controllers/api/users.php
@@ -4,6 +4,29 @@ require_once(APPPATH.'libraries/REST_Controller.php');
 
 class Users extends REST_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$token_value = $this->get('token');
+
+		$token = new Token();
+		$token->where('value',$token_value);
+		$token->get();
+
+		if (!$token->exists())
+		{
+			$this->response(
+				array(
+					'error'=>'Invalid Token'
+				)
+			);
+		}
+		
+		
+		$this->user = $token->user;
+		$this->application = $token->application;
+	}
+
 	public function index_get()
 	{
 		$id = $this->get('id');
@@ -21,7 +44,22 @@ class Users extends REST_Controller {
 			)
 		);
 	}
-	
+
+
+	public function me_get()
+	{
+		$user = $this->user->get();
+		
+		$this->response(
+			array(
+				'id'=>$user->id,
+				'firstname'=>$user->firstname,
+				'middlename'=>$user->middlename,
+				'lastname'=>$user->lastname,
+				'email'=>$user->email
+			)
+		);
+	}
 }
 
 /* End of file api.php */
