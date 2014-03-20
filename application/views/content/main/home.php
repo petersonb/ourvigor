87036633656bd2c0a6dd4a76c2b8dbcbdb12dec7
Brett Peterson <bepeterson14@gmail.com>
2014-03-20 07:06:07 -0500

    updated home page

--- application/views/content/main/home.php
+++ application/views/content/main/home.php
@@ -1,7 +1,5 @@
 <h1>Welcome to OurVigor!</h1>
-<p>This website is still under extreme development, and at the moment really doesn't do anything useful! Check back later for a new fitness social netorking experience.</p>
-<br />
-<p>Also, yes the color scheme is quite interesting (for now). Just part of the extreme development process!</p>
+<p>This website is under heavy development. However, still feel free to make an account! You can also check out our <a href="<?php echo base_url('development'); ?>">development page</a>, where you can submit your own ideas for what you would like to see from a fitness social networking web application. You can also read my development logs to see what I've been up to.</p>
 <div>
     <h3>Login</h3>
     <?php $this->load->view('forms/users/login'); ?>
