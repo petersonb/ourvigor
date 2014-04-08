25f37a4e44ef643df5f2835c2911079c70c24b4b
Brett Peterson <bepeterson14@gmail.com>
2014-04-07 23:44:56 -0500

    updated existing account error

--- application/views/content/errors/fb/existing_account.php
+++ application/views/content/errors/fb/existing_account.php
@@ -1,1 +1,1 @@
-<p>There is an existing user already linked with this Facebook account. If this is your account, please log in and link your account.</p>
+<p>There is an existing user already linked with this Facebook account, or using this email. If this is your account, please log in and link your account.</p>
