60b593cb6a15f116895255e8625bfd6246157e2c
Brett Peterson <bepeterson14@gmail.com>
2014-06-26 16:53:42 -0500

    updated edit profile page

--- application/views/forms/profiles/edit.php
+++ application/views/forms/profiles/edit.php
@@ -13,26 +13,53 @@ $about_val = $profile['about'];
 
 <?php echo validation_errors(); ?>
 <?php echo form_open('profiles/edit'); ?>
-<?php
-
-$this->table->add_row('Gender', form_input('gender', $gender_val));
-
-$birthday_params = array (
-	'name'  => 'date_of_birth',
-	'id'    => 'datepicker',
-	'value' => $dob_val
-);
-
-$this->table->add_row('Birth Date', form_input($birthday_params));
-$this->table->add_row('Phone', form_input('phone', $phone_val));
-$this->table->add_row('Phone (EXT)', form_input('phone_ext', $phone_ext_val));
-$this->table->add_row('Street 1', form_input('street_1', $street1_val));
-$this->table->add_row('Street 2', form_input('street_2', $street2_val));
-$this->table->add_row('City', form_input('city', $city_val));
-$this->table->add_row('State', form_input('state', $state_val));
-$this->table->add_row('Zip', form_input('zip', $zip_val));
-$this->table->add_row('About', form_input('about', $about_val));
-$this->table->add_row(form_submit('submit', 'Save Changes'),'');
-echo $this->table->generate();
-?>
+<table>
+    <tr>
+	<td>Gender</td>
+	<td>
+	    <select name="gender">
+		<option <?php if ($gender_val == 'm'): ?>selected="selected" <?php endif; ?> value="m">Male</option>
+		<option <?php if ($gender_val == 'f'): ?>selected="selected" <?php endif; ?> value="f">Female</option>
+		<option <?php if ($gender_val != 'm' && $gender_val != 'f'): ?>selected="selected" <?php endif; ?> value="n">Not Specified</option>
+	    </select>
+	</td>
+    </tr>
+    <tr>
+	<td>Birth Date</td>
+	<td><input type="text" id="datepicker" name="date_of_birth" value="<?php echo $dob_val; ?>" /></td>
+    </tr>
+    <tr>
+	<td>Phone</td>
+	<td><input type="text" name="phone" value="<?php echo $phone_val; ?>" /></td>
+    </tr>
+    <tr>
+	<td>Phone (EXT)</td>
+	<td><input type="text" name="phone_ext" value="<?php echo $phone_ext_val; ?>" /></td>
+    </tr>
+    <tr>
+	<td>Street 1</td>
+	<td><input type="text" name="street_1" value="<?php echo $street1_val; ?>" /></td>
+    </tr>
+    <tr>
+	<td>Street 2</td>
+	<td><input type="text" name="street_2" value="<?php echo $street2_val; ?>" /></td>
+    </tr>
+    <tr>
+	<td>City</td>
+	<td><input type="text" name="city" value="<?php echo $city_val; ?>" /></td>
+    </tr>
+    <tr>
+	<td>State</td>
+	<td><input type="text" name="state" value="<?php echo $state_val; ?>" /></td>
+    </tr>
+    <tr>
+	<td>Zip</td>
+	<td><input type="text" name="zip" value="<?php echo $zip_val; ?>" /></td>
+    </tr>
+    <tr>
+	<td>About</td>
+	<td><textarea><?php echo $about_val; ?></textarea></td>
+    </tr>
+</table>
+<input type="submit" value="Save" />
 </form>
